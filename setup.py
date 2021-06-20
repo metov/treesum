3ebbdd59a256eca88bedadd3b1a700f78e486b2b
Azat <azatinfo@yandex.com>
2021-06-19 21:55:52 -0700

    add description, url to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
+from pathlib import Path
 
 setup(
     name="treesum",
     version="0.3.1",
-    description="Utility and library for summarizing trees.",
     author="Azat Akhmetov",
     author_email="azatinfo@yandex.com",
+    description="Utility and library for summarizing trees.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/metov/treesum",
     classifiers=["Development Status :: 3 - Alpha"],
     install_requires=[
         "docopt",
