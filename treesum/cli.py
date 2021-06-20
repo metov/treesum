5bc7e4ede5a61190a11f1bd9c7962f9b4e56c9ec
Azat <azatinfo@yandex.com>
2021-06-19 21:04:04 -0700

    add paths subcommand to cli

--- treesum/cli.py
+++ treesum/cli.py
@@ -2,6 +2,7 @@
 Summarize trees from the command line.
 
 Parses a tree in one of the following formats and summarizes it:
+* paths -- list of paths; as produced by eg. ls. Assumes weight of 1.
 * rsync -- rsync changeset, assumes a weight of 1 for every path
 
 The tree is read from standard input, results will go to standard output. See
@@ -9,6 +10,7 @@ https://github.com/metov/treesum for more information.
 
 Usage:
     treesum (-h | --help)
+    treesum paths [options]
     treesum rsync [options]
 
 Options:
@@ -26,11 +28,15 @@ def main():
     args = docopt(__doc__)
     n_lines = int(args["-n"]) or 10
 
-    tree = None
-    if args["rsync"]:
-        lines = sys.stdin.read().splitlines()
+    lines = sys.stdin.read().splitlines()
+    if args["paths"]:
+        tree = paths_to_tree(lines)
+    elif args["rsync"]:
         paths = rsync_to_paths(lines)
         tree = paths_to_tree(paths)
+    else:
+        # We should never end up here because docopt validates arguments
+        tree = None
 
     summary = summarize_tree(tree, n_lines)
 
